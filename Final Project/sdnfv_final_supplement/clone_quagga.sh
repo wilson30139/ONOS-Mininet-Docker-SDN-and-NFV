4c8ea3c7507660c1c004b80f3bc1416b9a5c5996
Wei-Cheng Lai <43327415+wilson30139@users.noreply.github.com>
2026-06-27 20:46:57 +0800

    Delete Final Project/sdnfv_final_supplement directory

--- Final Project/sdnfv_final_supplement/clone_quagga.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git clone --depth=1 --branch=cord-5.0 https://github.com/opencord/quagga.git
--- Final Project/sdnfv_final_supplement/dhcp_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo /usr/sbin/dhcpd 4 -pf /run/dhcp-server-dhcpd.pid -cf ./dhcpd.conf vethdhcpsovs1
